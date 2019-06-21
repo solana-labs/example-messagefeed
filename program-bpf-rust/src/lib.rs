730376d6c28d5972faff149772d3c6003480452f
Jack May <jack@solana.com>
2019-06-21 01:21:30 -0700

    nudge

--- program-bpf-rust/src/lib.rs
+++ program-bpf-rust/src/lib.rs
@@ -7,19 +7,18 @@ extern crate arrayref;
 extern crate solana_sdk_bpf_utils;
 
 use core::mem::size_of;
-use solana_sdk_bpf_utils::entrypoint;
 use solana_sdk_bpf_utils::entrypoint::*;
-use solana_sdk_bpf_utils::log::*;
+use solana_sdk_bpf_utils::{entrypoint, info};
 
 struct UserAccountData<'a> {
-    pub banned: &'a mut u8, // TODO How to make this a &bool without transmute?
+    pub banned: &'a mut bool,
     pub creator: &'a mut SolPubkey,
 }
 impl<'a> UserAccountData<'a> {
     fn new(data: &'a mut [u8]) -> Self {
         let (banned, creator) = data.split_at_mut(1);
         Self {
-            banned: &mut banned[0],
+            banned: unsafe { core::mem::transmute::<&mut u8, &mut bool>(&mut banned[0]) },
             creator: array_mut_ref!(creator, 0, size_of::<SolPubkey>()),
         }
     }
@@ -47,11 +46,11 @@ impl<'a> MessageAccountData<'a> {
 
 entrypoint!(process_instruction);
 fn process_instruction(ka: &mut [SolKeyedAccount], _info: &SolClusterInfo, data: &[u8]) -> bool {
-    sol_log("message feed entrypoint");
+    info!("message feed entrypoint");
 
     let len = ka.len();
     if len < 2 {
-        sol_log("Error: Expected at least two keys");
+        info!("Error: Expected at least two keys");
         return false;
     }
 
@@ -62,21 +61,21 @@ fn process_instruction(ka: &mut [SolKeyedAccount], _info: &SolClusterInfo, data:
     let new_message_data = MessageAccountData::new(message_account[0].data);
 
     if !user_account[0].is_signer {
-        sol_log("Error: not signed by key 0");
+        info!("Error: not signed by key 0");
         return false;
     }
     if !message_account[0].is_signer {
-        sol_log("Error: not signed by key 1");
+        info!("Error: not signed by key 1");
         return false;
     }
 
-    if *user_data.banned == 1 {
-        sol_log("Error: user is banned");
+    if *user_data.banned {
+        info!("Error: user is banned");
         return false;
     }
 
     // No instruction data means that a new user account should be initialized
-    if data.len() == 0 {
+    if data.is_empty() {
         user_data.creator.clone_from_slice(message_account[0].key);
         return true;
     }
@@ -92,7 +91,7 @@ fn process_instruction(ka: &mut [SolKeyedAccount], _info: &SolClusterInfo, data:
         let existing_message_data = MessageAccountData::new(existing_message_account[0].data);
 
         if existing_message_data.next_message != &[0; size_of::<SolPubkey>()] {
-            sol_log("Error: account 1 already has a next_message");
+            info!("Error: account 1 already has a next_message");
             return false;
         }
 
@@ -105,7 +104,7 @@ fn process_instruction(ka: &mut [SolKeyedAccount], _info: &SolClusterInfo, data:
         if len > 3 {
             let (ban_user_account, _) = rest.split_at_mut(1);
             let ban_user_data = UserAccountData::new(ban_user_account[0].data);
-            *ban_user_data.banned = 1;
+            *ban_user_data.banned = true;
         }
 
         // Propagate the chain creator to the new message
@@ -120,10 +119,10 @@ fn process_instruction(ka: &mut [SolKeyedAccount], _info: &SolClusterInfo, data:
     }
 
     if user_data.creator != new_message_data.creator {
-        sol_log("user_data/new_message_data creator mismatch");
+        info!("user_data/new_message_data creator mismatch");
         return false;
     }
 
-    sol_log("Success");
+    info!("Success");
     true
 }
