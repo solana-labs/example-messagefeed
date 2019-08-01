4bfa1d9aed0c09061f2cc6f9dcbb8755c25e22de
Justin Starry <justin@solana.com>
2019-08-01 11:20:56 -0400

    Bump @solana/web3.js and default to http

--- urls.js
+++ urls.js
@@ -3,7 +3,7 @@
 import {testnetChannelEndpoint} from '@solana/web3.js';
 
 export let url = process.env.LIVE
-  ? testnetChannelEndpoint(process.env.CHANNEL || 'beta')
+  ? testnetChannelEndpoint(process.env.CHANNEL || 'beta', false)
   : 'http://localhost:8899';
 
 export let walletUrl =
