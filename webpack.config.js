6014d0a7a6dc3d5f5a31cf23c0fafa09165442e9
Justin Starry <jstarry@users.noreply.github.com>
2019-08-15 16:38:18 -0400

    Pass through PORT env var to webapp

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         LIVE: JSON.stringify(process.env.LIVE),
+        PORT: JSON.stringify(process.env.PORT),
       },
     }),
   ],
