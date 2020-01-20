0a01f782cc46590b334e8cbac1a36c67f37edd18
Justin Starry <justin@solana.com>
2020-01-20 19:47:31 +0800

    Speed up config.json endpoint by not fetching program data (#314)

--- src/server/message-feed.js
+++ src/server/message-feed.js
@@ -17,6 +17,7 @@ export type MessageFeedMeta = {
 export default class MessageFeedController {
   meta: ?MessageFeedMeta;
   loading: boolean;
+  connection: Connection;
 
   constructor(connection: Connection) {
     this.connection = connection;
